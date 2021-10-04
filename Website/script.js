96190979be8664394b39a110c0bd9c8e562ee505
JabenJu <89491072+JabenJu@users.noreply.github.com>
2021-10-04 11:13:39 -0400

    New home page

---
+++ Website/script.js
@@ -0,0 +1,5 @@
+var grid_items = document.getElementsByClassName("grid-item");
+var grid_imgs = document.getElementsByClassName("hover-img")
+var pageX = document.getElementById("x");
+var pageY = document.getElementById("y");
+
